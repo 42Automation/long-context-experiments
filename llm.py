a0505ede3a56a09fcc24b23024635a261b2cd1f3
Adrian Tineo <adrian@adriantineo.com>
2025-09-05 16:35:55 +0200

    Add Poe LLM class

---
+++ llm.py
@@ -0,0 +1,65 @@
+import os
+from uuid import uuid4
+
+from dotenv import load_dotenv
+from fastapi_poe import (
+    ProtocolMessage,
+    QueryRequest,
+    get_final_response,
+    upload_file_sync,
+)
+from fastapi_poe.client import PROTOCOL_VERSION
+
+load_dotenv()
+
+api_key = os.environ.get("POE_API_KEY", "")
+if not api_key:
+    raise ValueError("Could not find POE_API_KEY variable in the environment")
+
+
+class LLM:
+    user_id: str = str(uuid4())
+
+    async def get_response(
+        self,
+        model: str,
+        query: str,
+        doc_urls: list[str] = [],
+        system_prompt: str | None = None,
+    ) -> str:
+        attachments = []
+        for doc_url in doc_urls:
+            attachment = upload_file_sync(file_url=doc_url, api_key=api_key)
+            attachments.append(attachment)
+
+        messages = []
+        if system_prompt:
+            messages.append(ProtocolMessage(role="system", content=system_prompt))
+        messages.append(
+            ProtocolMessage(role="user", content=query, attachments=attachments)
+        )
+
+        request = QueryRequest(
+            version=PROTOCOL_VERSION,
+            type="query",
+            query=messages,
+            user_id=self.user_id,
+            conversation_id="",
+            message_id="",
+            api_key=api_key,
+        )
+        response = await get_final_response(
+            request=request, bot_name=model, api_key=api_key
+        )
+        return response
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+        llm = LLM()
+        answer = await llm.get_response(model="GPT-5", query="Tell me about you")
+        print("Answer: " + answer)
+
+    asyncio.run(main())
