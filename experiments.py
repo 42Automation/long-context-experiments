6a6dbfaf334bbaa7dae32892f55002b011f3e9da
Adrian Tineo <adrian@adriantineo.com>
2025-09-05 18:48:22 +0200

    Run experiment

--- experiments.py
+++ experiments.py
@@ -1,26 +1,26 @@
 EXPERIMENTS = [
     {
-        "id": "segment_pdf",
-        "query": "my query",
+        "id": "segment_pdf_net_sales_japan",
+        "query": "What is the net sales for Japan in 2023?",
         "docs": [
             "./pdf/Apple_segment_operating_performance.pdf",
         ],
-        "expected_answer": "answer",
+        "expected_answer": "$24,257 million",
     },
     {
-        "id": "segment_md",
-        "query": "my query",
+        "id": "segment_md_net_sales_japan",
+        "query": "What is the net sales for Japan in 2023?",
         "docs": [
             "./md/Apple_segment_operating_performance.md",
         ],
-        "expected_answer": "answer",
+        "expected_answer": "$24,257 million",
     },
     {
-        "id": "segment_txt",
-        "query": "my query",
+        "id": "segment_txt_net_sales_japan",
+        "query": "What is the net sales for Japan in 2023?",
         "docs": [
             "./txt/Apple_segment_operating_performance.txt",
         ],
-        "expected_answer": "answer",
+        "expected_answer": "$24,257 million",
     },
 ]
