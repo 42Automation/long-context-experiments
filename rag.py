2fce957ad538b629ca56d28dfc4170df6a7ee85c
Adrian Tineo <adrian@adriantineo.com>
2025-09-19 18:07:40 +0200

    Revert "Refactor rag to use multiple retrievers (NOK)"
    51078b90617394bf22d0a66ef17603a6a64621e9

--- rag.py
+++ rag.py
@@ -17,45 +17,60 @@ def page_number(doc: Document) -> int:
     return doc.metadata["page_number"]
 
 
+def get_sorted_docs(filename: str, docs: list[Document]) -> list[Document]:
+    filtered = filter(lambda d: d.metadata["filename"] == filename, docs)
+    return sorted(filtered, key=page_number)
+
+
 class Retriever:
     vector_store: Chroma
-    filename: str
-    bm25_retriever: BM25Retriever
+    ensemble_retriever: EnsembleRetriever
+    pdf_doc_urls: list[str]
+    k: int
 
-    def __init__(self, pdf_doc_url: str):
+    def __init__(self, pdf_doc_urls: list[str], k: int):
         # Init vector store
+        print("Initalizing vector store...")
         self.vector_store = Chroma(
             collection_name="docs",
             persist_directory=CHROMA_FOLDER,
             embedding_function=embedding,
         )
 
-        # Load document
-        self.filename = Path(pdf_doc_url).stem
-        print(f"Loading document {self.filename}...")
-        with pdfplumber.open(pdf_doc_url) as pdf:
-            # Chunk by pages
-            for idx, page in enumerate(pdf.pages):
-                text = page.extract_text(layout=True)
-                doc = Document(
-                    page_content=text.strip(),
-                    metadata={"filename": self.filename, "page_number": idx + 1},
-                )
+        self.pdf_doc_urls = pdf_doc_urls
+        self.k = k
 
-            # Create BM25 retriever
-            self.bm25_retriever = BM25Retriever.from_documents([doc])
+        # Load documents
+        print("Building retriever...")
+        source_docs = []
+        for doc_url in pdf_doc_urls:
+            with pdfplumber.open(doc_url) as pdf:
+                # Chunk by pages
+                for idx, page in enumerate(pdf.pages):
+                    text = page.extract_text(layout=True)
+                    filename = Path(doc_url).stem
+                    source_docs.append(
+                        Document(
+                            page_content=text.strip(),
+                            metadata={"filename": filename, "page_number": idx + 1},
+                        )
+                    )
 
-            # Add documents to vector store, if needed
-            if not self._has_doc(doc):
-                self.vector_store.add_documents([doc])
+        # Create BM25 retriever
+        bm25_retriever = BM25Retriever.from_documents(source_docs)
+        bm25_retriever.k = k
 
-    def get_ensemble_retriever(self, k: int) -> EnsembleRetriever:
-        self.bm25_retriever.k = k
+        # Add documents to vector store, if needed
+        new_docs = [d for d in source_docs if (not self._has_doc(d)) and d.page_content]
+        if new_docs:
+            self.vector_store.add_documents(new_docs)
+
         semantic_retriever = self.vector_store.as_retriever(
-            search_kwargs={"k": k, "filter": {"filename": self.filename}}
+            search_kwargs={"k": k, "filter": {"filename": filename}}
         )
-        return EnsembleRetriever(
-            retrievers=[self.bm25_retriever, semantic_retriever], weights=[0.5, 0.5]
+
+        self.ensemble_retriever = EnsembleRetriever(
+            retrievers=[bm25_retriever, semantic_retriever], weights=[0.5, 0.5]
         )
 
     def _has_doc(self, doc: Document) -> bool:
@@ -64,25 +79,36 @@ class Retriever:
         has_doc = len(match["ids"]) > 0
         return has_doc
 
-    def get_relevant_documents(self, query: str, k: int) -> dict[str, Any]:
+    def get_relevant_documents(self, query: str) -> list[dict[str, Any]]:
         print("Getting relevant documents...")
-        docs = self.get_ensemble_retriever(k=k).invoke(query)
+        docs = self.ensemble_retriever.invoke(query)
 
-        return {
-            "filename": self.filename,
-            "pages": [
+        names = [Path(f).stem for f in self.pdf_doc_urls]
+        sorted_docs = []
+        for name in names:
+            sorted_docs.append(
                 {
-                    "page_number": d.metadata["page_number"],
-                    "text": d.page_content,
+                    "filename": name,
+                    "pages": [
+                        {
+                            "page_number": d.metadata["page_number"],
+                            "text": d.page_content,
+                        }
+                        for d in get_sorted_docs(name, docs)
+                    ],
                 }
-                for d in sorted(docs, key=page_number)
-            ],
-        }
+            )
+        return sorted_docs
 
 
 if __name__ == "__main__":
-    retriever = Retriever(pdf_doc_url="./pdf/Apple_SEC_filing_2024.pdf")
+    pdf_doc_urls = ["./pdf/Apple_SEC_filing_2024.pdf"]
+    retriever = Retriever(pdf_doc_urls=pdf_doc_urls, k=10)
     query = "How many employees does Apple have?"
-    result = retriever.get_relevant_documents(query=query, k=10)
-    for page in result["pages"]:
-        print(f"Page {page['page_number']}:\n{page['text']}")
+    docs = retriever.get_relevant_documents(query=query)
+
+    for doc in docs:
+        print(f"Filename: {doc['filename']}")
+        pages = doc["pages"]
+        for page in pages:
+            print(f"Page {page['page_number']}:\n{page['text']}")
