ed3ecb4d8ebd12e9ad76b4915683425fb7ef2b94
Adrian Tineo <adrian@adriantineo.com>
2025-09-20 15:37:36 +0200

    New experiment and results

--- experiments/LINK_cross_check_rag.py
+++ experiments/LINK_cross_check_rag.py
@@ -1,22 +1,22 @@
 EXPERIMENTS = [
     {
-        "id": "link_cross_check_rag_k10",
+        "id": "link_cross_check_rag_k5",
         "query": "Are there any discrepancies in the revenue and gross profit values between prospectus and annual report?",
         "reference_doc_urls": [
-            "./pdf/LINK Mobility IPO Prospectus Dec 10 2020.pdf",
+            "./pdf/LINK_prospectus_main.pdf",
             "./pdf/LINK Annual Report 2020.pdf",
         ],
-        "treatments": {"rag": {"k": 10}},
+        "treatments": {"rag": {"k": 5}},
         "expected_answer": "Yes, there are discrepancies. In particular, the revenue per segment for Central Europe in 2019 is 596,805K NOK in the annual report and 639,486K NOK in the prospectus, which also causes the totals to differ with 2,890,025K NOK in the annual report and 2,932,707K NOK in the prospectus.",
     },
     {
-        "id": "link_cross_check_rag_k20",
+        "id": "link_cross_check_rag_k10",
         "query": "Are there any discrepancies in the revenue and gross profit values between prospectus and annual report?",
         "reference_doc_urls": [
-            "./pdf/LINK Mobility IPO Prospectus Dec 10 2020.pdf",
+            "./pdf/LINK_prospectus_main.pdf",
             "./pdf/LINK Annual Report 2020.pdf",
         ],
-        "treatments": {"rag": {"k": 20}},
+        "treatments": {"rag": {"k": 10}},
         "expected_answer": "Yes, there are discrepancies. In particular, the revenue per segment for Central Europe in 2019 is 596,805K NOK in the annual report and 639,486K NOK in the prospectus, which also causes the totals to differ with 2,890,025K NOK in the annual report and 2,932,707K NOK in the prospectus.",
     },
 ]
